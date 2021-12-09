a06d87a32d3d299ed618fc507efe0a28f97a374a
kamleshjambaniugam <90193738+kamleshjambaniugam@users.noreply.github.com>
2021-12-09 18:09:22 +0530

    Service user commit

---
+++ core/src/main/java/com/project/core/models/ServiceUsers.java
@@ -0,0 +1,9 @@
+package com.personalproject.core.models;
+
+import javax.jcr.RepositoryException;
+
+public interface ServiceUsers {
+
+    public String getServiceUsers() throws RepositoryException;
+
+}
